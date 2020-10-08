7bbe77edf64e7c21268f53ebf0c309f661144762
hunglq7130 <hunglq7130@co-well.com.vn>
2020-10-08 08:31:53 +0700

    update

---
+++ src/main/java/com/luu/ubas/model/entity/DoctorMedicalCareLocation.java
@@ -0,0 +1,19 @@
+package com.luu.ubas.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+@Accessors(chain = true)
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class DoctorMedicalCareLocation {
+
+
+}
---
+++ src/main/java/com/luu/ubas/model/entity/DoctorSpecialization.java
@@ -0,0 +1,18 @@
+package com.luu.ubas.model.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Accessors(chain = true)
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class DoctorSpecialization {
+
+
+
+}
